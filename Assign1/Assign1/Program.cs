9f4c16e955dcbb0e43d692984eccff93118903bd
Priti Shreya <priti@promact.com>
2018-02-08 12:47:13 +0530

    third commit

---
+++ Assign1/Assign1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+
+namespace Assign1
+{
+    public class Product
+    {
+        public Product(string name, double price, int quantity, string type)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+            this.type = type;
+        }
+
+        public string name;
+        public double price;
+        public int quantity;
+        public string type;
+        public double range, range1, range2;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Your code goes here
+            ArrayList productList = new ArrayList();
+            productList.Add(new Product("lettuce", 10.5, 50, "Leafygreen"));
+            productList.Add(new Product("cabbage", 20, 100, "Cruciferous"));
+            productList.Add(new Product("pumpkin", 30, 30, "Marrow"));
+            productList.Add(new Product("cauliflower", 20, 100, "Cruciferous"));
+            productList.Add(new Product("zucchini", 20.5, 50, "marrow"));
+            productList.Add(new Product("yam", 30, 50, "Root"));
+            productList.Add(new Product("spinach", 10, 100, "leafygreen"));
+            productList.Add(new Product("brocolli", 20.2, 75, "Cruciferous"));
+            productList.Add(new Product("Garlic", 30, 20, "Leafygreen"));
+            productList.Add(new Product("silverbeet", 10, 50, "Marrow"));
+
+            Console.WriteLine("Total no. of product: " + productList.Count);
+            productList.Add(new Product("potato", 10, 50, "Root"));
+            foreach (Product item in productList)
+            {
+                Console.WriteLine("Name: " + item.name + "; price: " + item.price + "; quantity: " + item.quantity + "; type: " + item.type);
+            }
+            Console.WriteLine("Total no. of product: " + productList.Count);
+            foreach (Product item in productList)
+            {
+                if (item.type == "Leafygreen")
+                {
+                    Console.WriteLine("Name: " + item.name + "; price: " + item.price + "; quantity: " + item.quantity + "; type: " + item.type);
+                }
+            }
+            Console.WriteLine("If all the garlic are sold out..");
+            productList.RemoveAt(8);
+            foreach (Product item in productList)
+            {
+
+                Console.WriteLine("Name: " + item.name + "; price: " + item.price + "; quantity: " + item.quantity + "; type: " + item.type);
+
+            }
+            Console.WriteLine("If user add 50 cabbage in the inventory");
+            foreach (Product item in productList)
+            {
+                if (item.name == "cabbage")
+                {
+                    item.quantity = item.quantity + 50;
+                    Console.WriteLine("Name: " + item.name + "; price: " + item.price + "; quantity: " + item.quantity + "; type: " + item.type);
+                }
+            }
+            Console.WriteLine("If user purchases 1 kg lettuce,2 kg zucchini,1 kg broccoli");
+            double range=0;
+            foreach (Product item in productList)
+            {
+                if (item.name == "lettuce")
+                {
+                    range = item.price;
+                }
+                else if (item.name == "zucchini")
+                {
+                    range += 2 * item.price;
+                }
+                else if (item.name == "brocolli")
+                {
+                    range += item.price;
+                }
+            }
+                Console.WriteLine("Round amount user need to pay: " + range);
+
+            
+        }
+    }
+}
---
+++ Assign2/edo/edo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace edo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] edos= { "Boat", " house", "cat", "river", "cupboard" };
+                for (int i = 0; i < edos.Length; i++)
+                {
+                    edos[i] = edos[i] + "s";
+                    Console.WriteLine("Plurals are: " + edos[i]);
+                }
+            Console.WriteLine("After Replacing into Synonyms: ");
+            edos = String.Join(",", edos).Replace("house", "accomodation").Split(new Char[] { ',' });
+            foreach (String item in edos)
+            {
+                Console.WriteLine(item);   
+            }
+            Console.WriteLine("Adding new words in array and print the length: ");
+            edos[4]="Window";
+            Console.WriteLine("Array length: "+ edos.Length);
+            Console.WriteLine("Print the list of words which contains the length of characters as 7.");
+            for (int i = 0; i < edos.Length; i++)
+            {
+                //problem in this part
+                if (edos[i].Length==7)
+                Console.WriteLine(edos[i]);
+            }
+            Console.WriteLine("print word on 3 rd position");
+            edos[2] = "Doors";
+            for (int i = 0; i < edos.Length; i++)
+            {
+                
+                Console.WriteLine("print the array " + edos[i]);
+            }
+            Console.WriteLine("sort the array:");
+            Array.Sort(edos);
+            for (int i = 0; i < edos.Length; i++)
+            {
+
+                Console.WriteLine("print the array " + edos[i]);
+            }
+            Console.WriteLine("reverse the array:");
+            Array.Reverse(edos);
+            for (int i = 0; i < edos.Length; i++)
+            {
+
+                Console.WriteLine("print the array " + edos[i]);
+            }
+        } 
+                
+        }
+        
+        }
+    
+
